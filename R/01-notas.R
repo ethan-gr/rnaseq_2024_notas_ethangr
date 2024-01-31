3492042f7a9ff939b8ed6f5d51959a2f075d098d
ethan-gr <ethangr@lcg.unam.mx>
2024-01-31 00:31:42 -0600

    Initial commit

---
+++ R/01-notas.R
@@ -0,0 +1,31 @@
+# NOTES 01
+
+# booktab package?
+# github static web page
+
+# `ctrl + .` Rsutdio
+
+# BIOCONDUCTOR
+
+# Born for massive biologic data analysis
+# A free slack with different util channels
+# Packages that interact with different databases for analysis
+# Software packages cannot weight > 5M, but experiment packages can
+# Orchestrating single cell analysis: Its a book to identify different packages for a same thing and its differences
+# Bioconductor tree: A tool to find packages
+# A same package can be in different tables
+
+# Important tags: support, build, dependencies
+
+# BiocManager
+# Obligatory extra information
+# Installation
+# How ask for help?
+# Need to know
+# How to cite?
+# Quick Start
+#
+# biocViews
+# Has a git server, not github: code.bioconductor
+# Updated every 6 months: April & October
+# Support site of Bioconductor
